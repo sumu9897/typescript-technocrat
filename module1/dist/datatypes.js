cc08c800857a2aca1f869ba3c94ffb40c27d3d63
sumu9897 <sumon.bncd@gmail.com>
2025-05-02 21:38:21 +0600

    Basic data types

---
+++ module1/dist/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// Basic Data Type
+// String
+let firstName = "Sumon";
+// number
+let roll = 9897;
+// boolean
+let isStudent = true;
+// undefined
+let x = undefined;
+// null
+let y = null;
+let d;
+d = 123;
+// Array 
+let friends = ['shibly', 'asif'];
+friends.push('sakib');
+let eligibleRollList = [1, 2, 4, 5, 7, 10];
+// tuple  --> array --> order --> type of values
+let coordinate = [1, 5];
+let nameAge = ['Mr X', 34, 101, true];
+nameAge[1] = 35;
---
+++ module1/src/datatypes.ts
@@ -0,0 +1,32 @@
+// Basic Data Type
+
+// String
+let firstName: string = "Sumon";
+
+// number
+let roll: number = 9897;
+
+// boolean
+let isStudent: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+let d : number;
+d = 123;
+
+
+// Array 
+let friends : string[] = ['shibly', 'asif']
+friends.push('sakib')
+let eligibleRollList : number[] = [1,2,4,5,7,10]
+
+// tuple  --> array --> order --> type of values
+ let coordinate : [number, number]= [1,5]
+
+ let nameAge : [string, number, number, boolean] = ['Mr X', 34, 101, true]
+
+ nameAge[1]= 35
