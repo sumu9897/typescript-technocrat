634c9532bfb825097f52d03236e790fff3dd7625
sumu9897 <sumon.bncd@gmail.com>
2025-05-02 22:00:26 +0600

    Object, Optional and Literal Types

---
+++ module1/src/object.ts
@@ -0,0 +1,14 @@
+// Reference Type --> Object
+
+const user :{
+    firstName: string;
+    middleName?: string; // Optional Type
+    lastName: string;
+    isStudent: boolean;
+    unversity: 'American International University-Bangladesh'; // type --> literal types
+}= {
+    firstName: "Mohammad",
+    lastName: "Sumon",
+    isStudent: true,
+    unversity: "American International University-Bangladesh"
+}
