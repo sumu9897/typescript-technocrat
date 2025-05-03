da4047d62951e07e2f4c5c5395377b65b227b196
sumu9897 <sumon.bncd@gmail.com>
2025-05-03 14:13:15 +0600

    Type assertion / type narrowing

---
+++ module2/src/assertion.ts
@@ -0,0 +1,37 @@
+{
+    // type assertion
+    let anything : any;
+
+    anything = "Hello World";
+    anything = 222; 
+    // (anything  as number).
+
+    const kgToGm = (value: string | number) :string | number |undefined => {
+        if(typeof value === 'string'){
+            const convertedValue = parseFloat(value)*100
+            return ` The converted value is :${convertedValue}`
+        }
+
+        if(typeof value === 'number'){
+            return value *1000
+        }
+
+        
+
+    }
+    const result1 = kgToGm(1000) as number
+    const result2 = kgToGm('1000')
+    type CustomError = {
+        message: string
+    }
+
+    try {
+        
+    } catch (error) {
+        console.log((error as CustomError).message);
+        
+    }
+
+    console.log(result1, result2);
+    
+}
